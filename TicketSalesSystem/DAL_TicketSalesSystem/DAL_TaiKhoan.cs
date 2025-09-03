bb8f5084a4e09ceeac1e9c0a19532803957f66ba
namvfg <duakhongchau1234@gmail.com>
2025-09-04 00:02:07 +0700

    xong Tai Khoan va Nguoi Dung

---
+++ TicketSalesSystem/DAL_TicketSalesSystem/DAL_TaiKhoan.cs
@@ -0,0 +1,67 @@
+﻿using DTO_TicketSalesSystem;
+using DTO_TicketSalesSystem.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_TicketSalesSystem
+{
+    public class DAL_TaiKhoan
+    {
+        // Thêm tài khoản mới với mật khẩu được băm
+        public bool ThemTaiKhoan(DTO_TaiKhoan dto)
+        {
+            using (var ctx = new TicketSalesContext())
+            {
+                try
+                {
+                    var tk = new TaiKhoan
+                    {
+                        TenDangNhap = dto.TenDangNhap,
+                        MatKhau = PasswordHasher.Hash(dto.MatKhau), // băm mật khẩu
+                        NgayTao = dto.NgayTao,
+                        TrangThai = dto.TrangThai.ToString(),
+                        MaNguoiDung = dto.MaNguoiDung
+                    };
+
+                    ctx.TaiKhoans.Add(tk);
+                    ctx.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Lỗi khi thêm tài khoản: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        // Đăng nhập với kiểm tra mật khẩu băm
+        public TaiKhoan DangNhap(string tenDangNhap, string matKhau)
+        {
+            using (var ctx = new TicketSalesContext())
+            {
+                string hashedPassword = PasswordHasher.Hash(matKhau); // băm mật khẩu người nhập
+
+                return ctx.TaiKhoans
+                          .FirstOrDefault(tk =>
+                              tk.TenDangNhap == tenDangNhap &&
+                              tk.MatKhau == hashedPassword &&
+                              tk.TrangThai == "HOATDONG");
+            }
+        }
+
+        // Kiểm tra tên đăng nhập có trùng không
+        public bool KiemTraTenDangNhapTrung(string tenDangNhap)
+        {
+            using (var ctx = new TicketSalesContext())
+            {
+                return ctx.TaiKhoans.Any(tk => tk.TenDangNhap == tenDangNhap);
+            }
+        }
+
+       
+    }
+}
--- TicketSalesSystem/GUI_TicketSalesSystem/FormLogin.Designer.cs
+++ TicketSalesSystem/GUI_TicketSalesSystem/FormLogin.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập vào hệ thống";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(40, 89);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(124, 24);
+            this.lbUsername.TabIndex = 1;
+            this.lbUsername.Text = "Tên tài khoản";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(40, 140);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(86, 24);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Mật khẩu";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(189, 84);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(235, 29);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(189, 135);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(235, 29);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(286, 193);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(138, 36);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(44, 193);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(138, 36);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Đăng ký";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(464, 257);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
 
