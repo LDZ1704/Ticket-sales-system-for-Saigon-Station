ee1e3502b6126f76324bf80cb772062a19f0b6e6
LDZ1704 <lamn9049@gmail.com>
2025-09-04 11:12:29 +0700

    update login/register/forgotpassword GUI

--- DatabaseBanVe.sql
+++ DatabaseBanVe.sql
@@ -7,24 +7,26 @@ GO
 USE BanVeGaSaiGon
 GO
 
+-- Bảng người dùng
+CREATE TABLE NguoiDung (
+    MaNguoiDung INT IDENTITY PRIMARY KEY,
+    Ho NVARCHAR(100) NOT NULL, --tach fullname
+	Ten NVARCHAR(50) NOT NULL, --tach fullname
+    NgaySinh DATE,
+    Email NVARCHAR(100),
+    SoDienThoai NVARCHAR(20),
+	NgayTao DATE DEFAULT GETDATE(), --them ngay tao
+    LoaiNguoiDung NVARCHAR(20) CHECK (LoaiNguoiDung IN ('KHACH','NHANVIEN','QUANTRI'))
+)
+
 -- Bảng tài khoản
 CREATE TABLE TaiKhoan (
     MaTaiKhoan INT IDENTITY PRIMARY KEY,
     TenDangNhap NVARCHAR(50) UNIQUE NOT NULL,
     MatKhau NVARCHAR(255) NOT NULL,
     NgayTao DATE DEFAULT GETDATE(),
-    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('HoatDong','Khoa'))
-)
-
--- Bảng người dùng
-CREATE TABLE NguoiDung (
-    MaNguoiDung INT IDENTITY PRIMARY KEY,
-    HoTen NVARCHAR(100) NOT NULL,
-    NgaySinh DATE,
-    Email NVARCHAR(100),
-    SoDienThoai NVARCHAR(20),
-    LoaiNguoiDung NVARCHAR(20) CHECK (LoaiNguoiDung IN ('Khach','NhanVien','QuanTri')),
-    MaTaiKhoan INT FOREIGN KEY REFERENCES TaiKhoan(MaTaiKhoan)
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('HOATDONG','KHOA')),
+	MaNguoiDung INT FOREIGN KEY REFERENCES NguoiDung(MaNguoiDung) --chuyển mã tài khoản của người dùng thành mã người dùng của tài khoản vì người dùng có thể đc tạo trước
 )
 
 -- Bảng hành khách
@@ -64,9 +66,8 @@ CREATE TABLE TuyenDuong (
 CREATE TABLE Tau (
     MaTau INT IDENTITY PRIMARY KEY,
     TenTau NVARCHAR(100),
-    MoTa NVARCHAR(200),
-    TongSoToa INT,
-    LoaiTau NVARCHAR(50)
+    MoTa NVARCHAR(200), --bỏ tổng số toa do truy vấn đc, 
+	--bỏ loại tàu do tàu này có 1 loại là chở khách thôi
 )
 
 -- Bảng toa tàu
@@ -74,8 +75,8 @@ CREATE TABLE ToaTau (
     MaToa INT IDENTITY PRIMARY KEY,
     TenToa NVARCHAR(50),
     LoaiGhe NVARCHAR(50),
-    SoLuongGhe INT,
-    ViTri NVARCHAR(50),
+    --bỏ sô lượng ghế do truy vấn đc
+    ViTri INT, --vị trí này là thứ tự thôi, nên để int
     MaTau INT FOREIGN KEY REFERENCES Tau(MaTau)
 )
 
@@ -84,7 +85,7 @@ CREATE TABLE Ghe (
     MaGhe INT IDENTITY PRIMARY KEY,
     SoHieu NVARCHAR(10),
     ViTri NVARCHAR(50),
-    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('Trong','DaDat','DangGiu')),
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('TRONG','DADAT')),
     MaToa INT FOREIGN KEY REFERENCES ToaTau(MaToa)
 )
 
@@ -93,10 +94,10 @@ CREATE TABLE ChuyenTau (
     MaChuyen INT IDENTITY PRIMARY KEY,
     MaTau INT FOREIGN KEY REFERENCES Tau(MaTau),
     MaTuyen INT FOREIGN KEY REFERENCES TuyenDuong(MaTuyen),
-    NgayKhoiHanh DATE,
-    GioXuatPhat TIME,
-    GioDen TIME,
-    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('MoBan','DaChay','Huy')),
+    GioKhoiHanh DATE,
+	-- bỏ giờ xuất phát do trong ngày khởi hành có cả giờ rồi
+    GioDen Date, -- chỉnh giờ đến thành Date luôn
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('MOBAN','DACHAY','HUY')),
     GhiChu NVARCHAR(200)
 )
 
@@ -104,24 +105,25 @@ CREATE TABLE ChuyenTau (
 CREATE TABLE ThanhToan (
     MaThanhToan INT IDENTITY PRIMARY KEY,
     MaNguoiDung INT FOREIGN KEY REFERENCES NguoiDung(MaNguoiDung),
-    HinhThuc NVARCHAR(50),
-    SoTien DECIMAL(12,2),
+    HinhThuc NVARCHAR(50) CHECK (HinhThuc IN ('VNPAY','MOMO')), --thêm enum cho cái này
+    --bỏ số tiền do truy vấn đc, với lại khi đổi trả vé nó có thể bị sai
     ThoiDiem DATETIME DEFAULT GETDATE(),
-    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('ThanhCong','ThatBai','DangXuLy'))
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('THANHCONG','THATBAI','DANGXULY')),
+	NgayThanhToan DATE DEFAULT GETDATE()
 )
 
 -- Bảng vé
 CREATE TABLE Ve (
     MaVe INT IDENTITY PRIMARY KEY,
-    MaNguoiDung INT FOREIGN KEY REFERENCES NguoiDung(MaNguoiDung),
+    --Bỏ mã người dùng vì nó nối với thanh toán rồi
     MaHanhKhach INT FOREIGN KEY REFERENCES HanhKhach(MaHanhKhach),
     MaChuyen INT FOREIGN KEY REFERENCES ChuyenTau(MaChuyen),
-    MaToa INT FOREIGN KEY REFERENCES ToaTau(MaToa),
+	-- bỏ mã toa vì nó có mã ghế rồi
     MaGhe INT FOREIGN KEY REFERENCES Ghe(MaGhe),
     MaThanhToan INT FOREIGN KEY REFERENCES ThanhToan(MaThanhToan),
-    NgayDat DATETIME DEFAULT GETDATE(),
+    -- bỏ ngày đặt vì đã lưu trong thanh toán
     GiaVe DECIMAL(12,2),
-    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('GiuCho','DaThanhToan','DaHuy','DaDoi')),
+    TrangThai NVARCHAR(20) CHECK (TrangThai IN ('DATHANHTOAN','DAHUY','DADOI')),
     MaQR NVARCHAR(100)
 )
 
---
+++ TicketSalesSystem/GUI_TicketSalesSystem/FormRegister.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace GUI_TicketSalesSystem
+{
+    partial class FormRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.lbBirthday = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbPhone = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnBackLogin = new System.Windows.Forms.Button();
+            this.dateNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng ký tài khoản";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(13, 58);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(124, 24);
+            this.lbUsername.TabIndex = 1;
+            this.lbUsername.Text = "Tên tài khoản";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(13, 96);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(86, 24);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Mật khẩu";
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFirstName.Location = new System.Drawing.Point(13, 132);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(35, 24);
+            this.lbFirstName.TabIndex = 1;
+            this.lbFirstName.Text = "Họ";
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLastName.Location = new System.Drawing.Point(12, 168);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(44, 24);
+            this.lbLastName.TabIndex = 1;
+            this.lbLastName.Text = "Tên";
+            // 
+            // lbBirthday
+            // 
+            this.lbBirthday.AutoSize = true;
+            this.lbBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBirthday.Location = new System.Drawing.Point(12, 204);
+            this.lbBirthday.Name = "lbBirthday";
+            this.lbBirthday.Size = new System.Drawing.Size(94, 24);
+            this.lbBirthday.TabIndex = 1;
+            this.lbBirthday.Text = "Ngày sinh";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmail.Location = new System.Drawing.Point(12, 239);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(57, 24);
+            this.lbEmail.TabIndex = 1;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.AutoSize = true;
+            this.lbPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPhone.Location = new System.Drawing.Point(12, 273);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(121, 24);
+            this.lbPhone.TabIndex = 1;
+            this.lbPhone.Text = "Số điện thoại";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(143, 53);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(316, 29);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(143, 88);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(316, 29);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(143, 123);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(316, 29);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(143, 163);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(316, 29);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(143, 234);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(316, 29);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(143, 268);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(316, 29);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // btnBackLogin
+            // 
+            this.btnBackLogin.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnBackLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackLogin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBackLogin.Location = new System.Drawing.Point(17, 322);
+            this.btnBackLogin.Name = "btnBackLogin";
+            this.btnBackLogin.Size = new System.Drawing.Size(200, 44);
+            this.btnBackLogin.TabIndex = 3;
+            this.btnBackLogin.Text = "Quay về đăng nhập";
+            this.btnBackLogin.UseVisualStyleBackColor = false;
+            // 
+            // dateNgaySinh
+            // 
+            this.dateNgaySinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateNgaySinh.Location = new System.Drawing.Point(143, 199);
+            this.dateNgaySinh.Name = "dateNgaySinh";
+            this.dateNgaySinh.Size = new System.Drawing.Size(316, 29);
+            this.dateNgaySinh.TabIndex = 4;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Lime;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRegister.Location = new System.Drawing.Point(328, 322);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(131, 44);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Đăng ký";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // FormRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 383);
+            this.Controls.Add(this.dateNgaySinh);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnBackLogin);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lbPhone);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbBirthday);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "FormRegister";
+            this.Text = "Đăng ký";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.Label lbBirthday;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbPhone;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnBackLogin;
+        private System.Windows.Forms.DateTimePicker dateNgaySinh;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
